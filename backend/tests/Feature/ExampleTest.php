800063c74618339941071824a56336f789c7d8f3
taoluan <luan.tao@cadabra.jp>
2023-08-02 15:45:02 +0700

    unit test fail

--- backend/tests/Feature/ExampleTest.php
+++ backend/tests/Feature/ExampleTest.php
@@ -14,6 +14,7 @@ class ExampleTest extends TestCase
      */
     public function test_example()
     {
-        $this->assertTrue(true);
+//        $this->assertTrue(true);
+        $this->assertEquals(10,11);
     }
 }
