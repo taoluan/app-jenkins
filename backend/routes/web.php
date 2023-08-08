f94e402d79e57efef2f87ddd3cf5f15521a3a1b3
taoluan <luan.tao@cadabra.jp>
2023-08-08 14:10:32 +0700

    sonar-script-13

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
