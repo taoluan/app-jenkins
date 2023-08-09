aaccc509d0e9ecc86e5a8390ec2a257b3bb441c9
taoluan <luan.tao@cadabra.jp>
2023-08-09 10:36:05 +0700

    sonar-1

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -14,14 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    312313
-    $r
-    return view('welcome');
-    return view('welcome');
-    return view('welcome');
-    return view('welcome');
-    return view('welcome');
-    return view('welcome');
-    return view('welcome');
     return view('welcome');
 });
