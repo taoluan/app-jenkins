d9ca468a265c234852b0b2c32dc69709b00b850f
luantaocadabra <luan.tao@cadabra.jp>
2023-08-28 14:59:11 +0700

    new-1

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -14,5 +14,37 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    $target = -5;
+    $num = 3;
+    $target =- $num; 
+    $target =+ $num; 
+    $target = -5;
+    $num = 3;
+    $target =- $num; 
+    $target =+ $num; 
+    $target = -5;
+    $num = 3;
+    $target =- $num; 
+    $target =+ $num; 
+    $target = -5;
+    $num = 3;
+    $target =- $num; 
+    $target =+ $num; 
+    $target = -5;
+    $num = 3;
+    $target =- $num; 
+    $target =+ $num; 
+    $target = -5;
+    $num = 3;
+    $target =- $num; 
+    $target =+ $num; 
+    $target = -5;
+    $num = 3;
+    $target =- $num; 
+    $target =+ $num; 
+    $target = -5;
+    $num = 3;
+    $target =- $num; 
+    $target =+ $num; 
     return view('welcome');
 });
