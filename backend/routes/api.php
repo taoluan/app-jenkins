dad477e306a5743733e2070e25760213df4aa42b
luantaocadabra <luan.tao@cadabra.jp>
2023-08-28 15:01:25 +0700

    new-2

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -15,29 +15,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    // $servername = "localhost";
-    // $username = "your_username";
-    // $password = "your_password";
-    // $dbname = "your_database";
-
-    // // Tạo kết nối đến cơ sở dữ liệu
-    // $conn = new mysqli($servername, $username, $password, $dbname);
-    $target = -5;
-    $num = 3;
-    $target =- $num; 
-    $target =+ $num; 
-    // $resultAnd = true and false; 
-    // $userInput = $_GET['username'];
-    // // Sanitize user input to prevent SQL injection
-    // $cleanInput = mysqli_real_escape_string($conn, $userInput);
-    // $query = "SELECT * FROM users WHERE username = '$cleanInput'";
-    // mysqli_query($conn, $query);
-    $user = $_POST['username'];
-    $pass = $_POST['password'];
-    if ($user == 'admin' && $pass == 'admin123') {
-        // Grant access
-    }
-    $userInput = $_GET['search'];
-    echo "<div>" . $userInput . "</div>";
     return $request->user();
 });
--- backend/routes/web.php
+++ backend/routes/web.php
@@ -14,37 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    $target = -5;
-    $num = 3;
-    $target =- $num; 
-    $target =+ $num; 
-    $target = -5;
-    $num = 3;
-    $target =- $num; 
-    $target =+ $num; 
-    $target = -5;
-    $num = 3;
-    $target =- $num; 
-    $target =+ $num; 
-    $target = -5;
-    $num = 3;
-    $target =- $num; 
-    $target =+ $num; 
-    $target = -5;
-    $num = 3;
-    $target =- $num; 
-    $target =+ $num; 
-    $target = -5;
-    $num = 3;
-    $target =- $num; 
-    $target =+ $num; 
-    $target = -5;
-    $num = 3;
-    $target =- $num; 
-    $target =+ $num; 
-    $target = -5;
-    $num = 3;
-    $target =- $num; 
-    $target =+ $num; 
     return view('welcome');
 });
