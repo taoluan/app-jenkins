127232d69505623c3c0b896791f64ee87927352e
luantaocadabra <luan.tao@cadabra.jp>
2023-08-28 15:11:15 +0700

    sonar-3

--- backend/app/Http/Controllers/Controller.php
+++ backend/app/Http/Controllers/Controller.php
@@ -17,5 +17,25 @@ class Controller extends BaseController
 
     public function foo($a) { // Noncompliant, function will return "true" or null
         $curl = 21313;
+        $MessageFile = "messages.out";
+            if ($_GET["action"] == "NewMessage") {
+            $name = $_GET["name"];
+            $message = $_GET["message"];
+            $handle = fopen($MessageFile, "a+");
+            fwrite($handle, "<b>$name</b> says '$message'<hr>\n");
+            fclose($handle);
+            echo "Message Saved!<p>\n";
+            }
+            else if ($_GET["action"] == "ViewMessages") {
+            include($MessageFile);
+            }
       }
+
+      public  function encryptPassword($password){
+        $iv_size = mcrypt_get_iv_size(MCRYPT_DES, MCRYPT_MODE_ECB);
+        $iv = mcrypt_create_iv($iv_size, MCRYPT_RAND);
+        $key = "This is a password encryption key";
+        $encryptedPassword = mcrypt_encrypt(MCRYPT_DES, $key, $password, MCRYPT_MODE_ECB, $iv);
+        return $encryptedPassword;
+        }
 }
